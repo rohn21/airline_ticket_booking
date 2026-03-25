17f67c516c0a3f47c81d6c907073fcf7833335af
rohn21 <rohn2100@gmail.com>
2026-03-25 23:14:08 +0530

    Added url route for Booking

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     # path("", include("travel_core.urls")),
     path("", include("flights.urls")),
     path("", include("accounts.urls")),
-    # path("", include("bookings.urls")),
+    path("", include("bookings.urls")),
     # path("", include("payments.urls")),
 ]
